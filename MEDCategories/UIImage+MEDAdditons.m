a8e61b5b9d5cb4fd950162359571ca35b6297852
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2015-03-25 10:53:29 +0700

    Add UINavigationController, UIImage and NSArray categories

---
+++ MEDCategories/UIImage+MEDAdditons.m
@@ -0,0 +1,43 @@
+//
+//  UIImage+MEDAdditons.m
+//  MEDCategoriesDemo
+//
+//  Created by Vinh Nguyen on 25/03/2015.
+//  Copyright (c) Năm 2015 2359Media. All rights reserved.
+//
+
+#import "UIImage+MEDAdditons.h"
+
+@implementation UIImage (MEDAdditons)
+
+// Solution based on Ryan's answer: http://stackoverflow.com/a/9575252/1477298
+
++ (UIImage *)med_imageWithImage:(UIImage *)image scaledToSize:(CGSize)size
+{
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+        UIGraphicsBeginImageContextWithOptions(size, NO, [[UIScreen mainScreen] scale]);
+    } else {
+        UIGraphicsBeginImageContext(size);
+    }
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
++ (UIImage *)med_imageWithImage:(UIImage *)image scaledToMaxWidth:(CGFloat)width maxHeight:(CGFloat)height
+{
+    CGFloat oldWidth = image.size.width;
+    CGFloat oldHeight = image.size.height;
+    
+    CGFloat scaleFactor = (oldWidth > oldHeight) ? width / oldWidth : height / oldHeight;
+    
+    CGFloat newHeight = oldHeight * scaleFactor;
+    CGFloat newWidth = oldWidth * scaleFactor;
+    CGSize newSize = CGSizeMake(newWidth, newHeight);
+    
+    return [self med_imageWithImage:image scaledToSize:newSize];
+}
+
+@end
