5c9b80687c34b88e88d5508a5957534ffaf656c0
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2015-06-15 11:59:31 +0700

    Add NSLocale helper

---
+++ MEDCategories/NSLocale+MEDAdditions.h
@@ -0,0 +1,15 @@
+//
+//  NSLocale+MEDAdditions.h
+//  Pods
+//
+//  Created by Vinh Nguyen on 15/06/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSLocale (MEDAdditions)
+
++ (NSString *)med_deviceLanguageCode;
+
+@end
