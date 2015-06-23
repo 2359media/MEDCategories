1d0a6366c7eebfc49d4ee8ef8720dd260d176e21
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2015-06-23 12:21:18 +0700

    Add NSData MD5 hash function

--- MEDCategories/NSData+MEDAdditions.m
+++ MEDCategories/NSData+MEDAdditions.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSData+MEDAdditions.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation NSData (MEDAdditions)
 
@@ -36,6 +37,19 @@
     }
 
     return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
+}
+
+- (NSString *)md5
+{
+    unsigned char result[16];
+    CC_MD5(self.bytes, (CC_LONG)self.length, result);
+    
+    return [NSString stringWithFormat: @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
 }
 
 @end
