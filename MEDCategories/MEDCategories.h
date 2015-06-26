7337e115d9075cda6b96b89d8f17ece79420ca07
Vinh Nguyen <vinh.nguyenxuan@2359media.com>
2015-06-26 17:58:05 +0700

    Add top header file

---
+++ MEDCategories/MEDCategories.h
@@ -0,0 +1,34 @@
+//
+//  MEDCategories.h
+//  Pods
+//
+//  Created by Vinh Nguyen on 26/06/2015.
+//
+//
+
+/// Foundation Categories
+#import "NSArray+MEDAdditions.h"
+#import "NSCalendar+MEDAdditions.h"
+#import "NSData+MEDAdditions.h"
+#import "NSDate+MEDAdditions.h"
+#import "NSDictionary+MEDAdditions.h"
+#import "NSLocale+MEDAdditions.h"
+#import "NSNumber+MEDAdditions.h"
+#import "NSObject+MEDAdditions.h"
+#import "NSSet+MEDAdditions.h"
+#import "NSString+MEDAdditions.h"
+#import "NSString+MEDCrypto.h"
+#import "NSURL+MEDAdditions.h"
+#import "NSUserDefaults+MEDAdditions.h"
+
+/// UIKit Categories
+#import "UIColor+MEDAdditions.h"
+#import "UIImage+MEDAdditions.h"
+#import "UINavigationController+MEDAdditions.h"
+#import "UIView+MEDAdditions.h"
+#import "UIView+MEDAutoLayout.h"
+#import "UIViewController+MEDAdditions.h"
+#import "UIWebView+MEDAdditions.h"
+
+/// Mantle Categories
+#import "MTLModel+MEDAdditions"
